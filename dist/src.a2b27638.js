5c6517d04f392166d2b8bcdcb5e288e9603643e5
Jona <jonastyle1990@gmail.com>
2023-01-13 13:45:12 +0000

    とりあえずletとconstの使い方を学んだ。letは今後上書き、再定義を行う可能性があるもの。constはそうじゃない時に使う。が、constでもオブジェクトのプロパティ、配列のプロパティを後から上書き、追加することはできる。

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -126,6 +126,54 @@ var val1 = "var変数";
 console.log(val1);
 val1 = "var変数を上書き";
 console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "再宣言しましたよ";
+console.log(val1);
+
+/**
+ * var 変数は上書きできるからこそ、大きなプロジェクトで扱いづらくなってくるらしい。
+ */
+
+var val2 = "let変数";
+console.log(val2);
+
+// letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可能!!!!!!!!!varと違うね
+// let val2 = "let変数を再宣言しamした";
+//これでエラーが出ます
+
+var val3 = "const変数";
+console.log(val3);
+
+// const変数は上書きできません。再宣言もできません
+
+var val4 = {
+  name: "じゃけぇ",
+  age: 28
+};
+console.log(val4);
+// const でオブジェクトを定義した。
+
+// これ、上書きできないと思うやん？でも、オブジェクトのプロパティ(name/age)の上書きはできる。
+
+val4.name = "だけん"; // 上書き
+val4.address = "熊本"; // 追加
+// オブジェクトのプロパティにアクセスする場合には「.」ドットを入力する！！！！！！！
+console.log(val4);
+
+/**
+ * 続いて配列について...
+ * */
+
+// constで定義した配列はプロパティの変更が可能！！！！！！！
+var val5 = ["犬", "猫"];
+val5[0] = "さる"; // 1番目を猿を変更
+val5.push("monkey"); //配列のまったんに要素を追加
+console.log(val5);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -8,3 +8,50 @@ console.log(val1);
 val1 = "var変数を上書き";
 console.log(val1);
 
+// var変数は再宣言可能
+var val1 = "再宣言しましたよ";
+console.log(val1);
+
+/**
+ * var 変数は上書きできるからこそ、大きなプロジェクトで扱いづらくなってくるらしい。
+ */
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可能!!!!!!!!!varと違うね
+// let val2 = "let変数を再宣言しamした";
+//これでエラーが出ます
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書きできません。再宣言もできません
+
+const val4 = {
+  name: "じゃけぇ",
+  age: 28,
+};
+console.log(val4);
+// const でオブジェクトを定義した。
+
+// これ、上書きできないと思うやん？でも、オブジェクトのプロパティ(name/age)の上書きはできる。
+
+val4.name = "だけん"; // 上書き
+val4.address = "熊本"; // 追加
+// オブジェクトのプロパティにアクセスする場合には「.」ドットを入力する！！！！！！！
+console.log(val4);
+
+/**
+ * 続いて配列について...
+ * */
+
+// constで定義した配列はプロパティの変更が可能！！！！！！！
+const val5 = ["犬", "猫"];
+val5[0] = "さる"; // 1番目を猿を変更
+val5.push("monkey"); //配列のまったんに要素を追加
+console.log(val5);
